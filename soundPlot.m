75e1c1ce26df33e2e427ae0411164a26c7491d6d
Matthew Hanna <38821679+Matthew-Hanna@users.noreply.github.com>
2019-06-09 16:25:01 -0400

    fixed stuff

--- soundPlot.m
+++ soundPlot.m
@@ -3,8 +3,10 @@ mono = y(1:end, 1);
 samples = 1:size(mono);
 t = (1:size(mono))/fs;
 cosfunction = cos(1000*2*pi*t);
-sound(cosfunction, fs)
+#sound(cosfunction, fs)
 cosfunction = cosfunction(1: (fs/1000)*2);
 t = t(1:(fs/1000)*2);
+figure (1);
 plot(samples, mono)
+figure (2);
 plot(t, cosfunction)
