13bb24cb16303934caef8c2eee0caeeed4c187e5
thomasfortin1 <thomasfortin@live.com>
2019-06-08 20:34:10 -0400

    added alarms and sirens

--- Test_file.m
+++ Test_file.m
@@ -1,5 +1,5 @@
-[y,fs] = audioread("audiofiles/DEH_WTAW.ogg");
+[y,fs] = audioread("C:/Users/thoma/Desktop/2B/BME 252/BME252_CI_Project/audiofiles/Ambulance Siren Effect - Great Siren.ogg");
 mono = y(1:end,1) + y(1:end,2);
-mono = mono(1:661500);
+mono = mono(1:end);
 sound(mono,fs)
 %%
