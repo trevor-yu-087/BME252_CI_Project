ad8589e1565eff477ba494da2870938ab6d3b9d1
Thomas Fortin <38443322+thomasfortin1@users.noreply.github.com>
2019-07-27 21:29:58 -0400

    Updated Dimensions

--- Test Audio Files/LogScale.m
+++ Test Audio Files/LogScale.m
@@ -1,15 +1,15 @@
 function Frequencies = LogScale(N, Start, End, Coverage, Wing)
-    A = zeros(5, N);
+    A = zeros(N, 5);
     ScalingFactor = End/Start;
     for i = 0:(N-1)
        left = Start * ScalingFactor^(i/N);
        right = Start * ScalingFactor^((i+1)/N);
        start = (Coverage * (left - right) + ((Coverage * (left-right))^2 + 4 * right * left)^0.5)/2;
        stop = right * left / start;
-       A(2, i + 1) = start;
-       A(3, i + 1) = stop;
-       A(1, i + 1) = start * (start/stop)^Wing;
-       A(4, i + 1) = stop * (stop/start)^Wing;
+       A(i + 1, 2) = start;
+       A(i + 1, 3) = stop;
+       A(i + 1, 1) = start * (start/stop)^Wing;
+       A(i + 1, 4) = stop * (stop/start)^Wing;
     end
-    A(5, :) = (A(2, :) .* A(3, :)).^0.5;
+    A(:, 5) = (A(:, 2) .* A(:, 3)).^0.5;
 Frequencies = A;
